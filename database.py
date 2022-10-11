7fee87be6b2546b4288879e8228ffcaa38418df1
J1min <jmhong101506@gmail.com>
2022-10-12 00:54:01 +0900

    ADD :: 유저 단건조회 API 완성

--- database.py
+++ database.py
@@ -7,10 +7,8 @@ import json
 with open("./secret.json", "r") as f:
     userData = json.load(f)
 
-print(userData)
-DATABASE_URL = f"mysql+mysqlconnector://{userData['USERNAME']}:{userData['PASSWORD']}@{userData['HOSTNAME']}:{userData['PORT']}/{userData['DBNAME']}"
-print(DATABASE_URL)
+Base = declarative_base()
 
+DATABASE_URL = f"mysql+mysqlconnector://{userData['USERNAME']}:{userData['PASSWORD']}@{userData['HOSTNAME']}:{userData['PORT']}/{userData['DBNAME']}"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
