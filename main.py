cc3ac3cb1d82ac65736a41d433eaa489b69ba285
J1min <aoiharu41@gmail.com>
2022-10-27 11:21:11 +0900

    FIX :: Leads 전체조회 추가

--- main.py
+++ main.py
@@ -55,41 +55,25 @@ def get_user():
 @app.get("/user/{user_id}")  # 특정 유저 조회
 def get_user(user_id: int, db: Session = Depends(get_db)):
     users = db.query(model.info).filter(model.info.user_id == user_id).first()
-    return (
-        {"code": 404, "response": "유저가 없는데요"}
-        if users == None
-        else {"code": 200, "response": "유저가 있어요", "userData": users}
-    )
+    return {"code": 200, "response": "유저가 있어요", "userData": users}
 
 
 @app.get("/board/all")  # 전체 게시글 조회
 def get_all_board(db: Session = Depends(get_db)):
     board = db.query(model.board).all()
-    return (
-        {"code": 404, "response": "게시글이 하나도 없는데요"}
-        if board == None
-        else {"code": 200, "response": "게시글이 있어요", "boardData": board}
-    )
+    return {"code": 200, "response": "게시글이 있어요", "boardData": board}
 
 
 @app.get("/board/{board_id}")  # 특정 게시글 조회
 def get_board(board_id: int, db: Session = Depends(get_db)):
     board = db.query(model.board).filter(model.board.board_id == board_id).first()
-    return (
-        {"code": 404, "response": "게시글이 없는데요"}
-        if board == None
-        else {"code": 200, "response": "게시글이 있어요", "boardData": board}
-    )
+    return {"code": 200, "response": "게시글이 있어요", "boardData": board}
 
 
 @app.get("/script/all")
 def get_all_script(db: Session = Depends(get_db)):
     script = db.query(model.script).all()
-    return (
-        {"code": 404, "response": "명언이 하나도 없는데요"}
-        if script == None
-        else {"code": 200, "response": "명언이 있어요", "scriptData": result}
-    )
+    return {"code": 200, "response": "명언이 있어요", "scriptData": script}
 
 
 @app.get("/script/random")
@@ -100,46 +84,38 @@ def get_all_script(db: Session = Depends(get_db)):
         "script_content": script[randomNumber].script_content,
         "author": script[randomNumber].author,
     }
-    return (
-        {"code": 404, "response": "명언이 하나도 없는데요"}
-        if script == None
-        else {"code": 200, "response": "명언이 있어요", "scriptData": result}
-    )
+    return {"code": 200, "response": "명언이 있어요", "scriptData": result}
 
 
 @app.get("/script/{script_id}")  # 특정 명언 조회
 def get_script(script_id: int, db: Session = Depends(get_db)):
     script = db.query(model.script).filter(model.script.script_id == script_id).first()
-    return (
-        {"code": 404, "response": "명언이 없는데요"}
-        if script == None
-        else {"code": 200, "response": "명언이 있어요", "scriptData": script}
-    )
+    return {"code": 200, "response": "명언이 있어요", "scriptData": script}
+
+
+@app.get("/user/leads/all")  # leads 조회
+def get_leads(db: Session = Depends(get_db)):
+    userData = db.query(model.leads).all()
+    return {"code": 200, "response": "전송 완료", "userData": userData}
 
 
 @app.post("/script")  # 명언 작성
-async def post_board(body: schemas.script, db: Session = Depends(get_db)):
+def post_board(body: schemas.script, db: Session = Depends(get_db)):
     scriptData = model.script(script_content=body.script_content, author=body.author)
-    try:
-        post_db(db, scriptData)
-        return {"code": 200, "response": "추가 완료", "Data": scriptData}
-    except:
-        return {"code": 404, "response": "추가 실패"}
+    post_db(db, scriptData)
+    return {"code": 200, "response": "추가 완료", "Data": scriptData}
 
 
 @app.post("/write")  # 게시글 작성
-async def post_board(body: schemas.board, db: Session = Depends(get_db)):
+def post_board(body: schemas.board, db: Session = Depends(get_db)):
     boardData = model.board(
         content=body.content,
         created_at=body.created_at,
         board_title=body.board_title,
         user_id=body.user_id,
     )
-    try:
-        post_db(db, boardData)
-        return {"code": 200, "response": "전송 완료", "boardData": boardData}
-    except:
-        return {"code": 404, "response": "전송이 안됨"}
+    post_db(db, boardData)
+    return {"code": 200, "response": "전송 완료", "boardData": boardData}
 
 
 def upload_file(fileName, file):
@@ -156,8 +132,17 @@ async def upload_photo(file: UploadFile, db: Session = Depends(get_db)):
     href = f"{str(uuid.uuid4())}.jpeg"  # uuid로 유니크한 파일명으로 변경
     upload_file(href, content)
     photoData = model.photos(href=href, board_id=1)
-    try:
-        post_db(db, photoData)
-        return photoData
-    except:
-        return {"code": 404, "response": "보드가 없는데요 뭔가이상"}
+    post_db(db, photoData)
+    return photoData
+
+
+@app.post("/user/leads")  # 게시글 작성
+def post_leads(body: schemas.leads, db: Session = Depends(get_db)):
+    userData = model.leads(
+        lead_name=body.lead_name,
+        started_at=body.started_at,
+        ended_at=body.ended_at,
+        user_id=1,
+    )
+    post_db(db, userData)
+    return {"code": 200, "response": "전송 완료", "userData": userData}
